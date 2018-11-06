0a15a29c7f74db1cf536c4870a6e72d31e014ac7
Farhad <ifarhadfaghihi@gmail.com>
2018-11-06 12:28:50 +0330

    added rxbinding dependencies definitions and the very first rx-code

--- app/src/main/java/io/github/farhad/courage/MainActivity.kt
+++ app/src/main/java/io/github/farhad/courage/MainActivity.kt
@@ -2,11 +2,35 @@ package io.github.farhad.courage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Consumer
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    var disposable : Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        disposable = Observable.just("Hello Farhad")
+            .observeOn(Schedulers.io())
+            .subscribeOn(AndroidSchedulers.mainThread())
+            .subscribe { s -> tvTitle.text = s }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable?.dispose()
     }
 }
